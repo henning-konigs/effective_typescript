82becb49ad35bcf65e666aa53d6e9f4f368afa50
hennong <henning.koenigs@gmx.de>
2025-05-03 16:21:25 +0200

    Finished Chapter 1 item 2

---
+++ chapters/chapter_one/item_two.ts
@@ -0,0 +1,19 @@
+// noImplicitAny
+
+// false
+// function test(a,b){
+//     return a+b
+// }
+
+// true
+function test(a: number, b: number) {
+  return a + b;
+}
+
+// strictNullChecks
+
+// false
+// const a: number = null;
+
+//true
+const b: number = 0;
